2e429013ad0dc8e0b4ae8d8f4879b961d49579c1
PhamDucPhuongMinh <phamducphuongminh0103@gmail.com>
2022-03-17 22:37:04 +0700

    Display 2 component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 import BookingMovieTicket from "./Component/BookingMovieTicket/BookingMovieTicket";
 import Glasses from "./Component/Glasses/Glasses";
 
 function App() {
+  const [isMovie, setIsMovie] = useState(false);
+  const changeExam = () => {
+    setIsMovie(!isMovie);
+  };
   return (
     <div className="App">
-      <BookingMovieTicket />
-      {/* <Glasses /> */}
+      <button className="btn btn-warning my-3" onClick={changeExam}>
+        Chuyển bài tập
+      </button>
+      {isMovie === true ? <BookingMovieTicket /> : <Glasses />}
     </div>
   );
 }
